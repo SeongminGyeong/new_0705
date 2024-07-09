b91b90be056d2695a7a36f1af63a322eced6a708
SeongminGyeong <smigy96@naver.com>
2024-07-09 11:06:11 +0900

    doc:수정 삭제

--- src/main/webapp/resources/sql/miniproj.sql
+++ src/main/webapp/resources/sql/miniproj.sql
@@ -28,3 +28,23 @@ select sha1(md5('1234'));
 -- Member테이블에 회원을 insert하는 쿼리문
 insert into member(userId, userPwd, userName, mobile, email)
 value(?,sha1(md5(?)),?,?,?);
+
+
+-- userId로 해당 유저의 정보를 검색하는 쿼리문
+select * from member where userId=?;
+
+-- 전체 정보를 검색하는 쿼리문
+select * from member;
+
+-- 둘리 회원의 이메일을 수정
+
+update member set email = 'ggaacc@gmail.com' where userId = 'dooly';
+
+
+-- ?회원 번화번호 변경 쿼리문
+update member set mobile = ?
+where userId = ? ;
+
+
+-- userId가 ?인 회원 삭제 (회원 탈퇴)
+delete from member where userId = ? ;
